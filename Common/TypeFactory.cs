3c472174b53dc17a05db9fee033fcf56e6223790
Anton Morokin <airbotxd@gmail.com>
2020-09-30 21:16:35 +0300

    Autofac IoC container was added

---
+++ Common/TypeFactory.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Common
+{
+    /// <summary>
+    /// Type factory.
+    /// </summary>
+    public static class TypeFactory
+    {
+        private static readonly Lazy<IContainer> __container
+            = new Lazy<IContainer>(InitializeContainer, System.Threading.LazyThreadSafetyMode.ExecutionAndPublication);
+
+        /// <summary>
+        /// Resolves instance of type <typeparamref name="T" /> from IoC container.
+        /// </summary>
+        /// <typeparam name="T">Type of instance.</typeparam>
+        /// <returns>Instance of type <typeparamref name="T"/></returns>
+        public static T Get<T>()
+        {
+            return __container.Value.Resolve<T>();
+        }
+
+        private static IContainer InitializeContainer()
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("autofac.json");
+
+            var module = new ConfigurationModule(configBuilder.Build());
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule(module);
+
+            return containerBuilder.Build();
+        }
+    }
+}
--- Resources/ResourceStore.cs
+++ Resources/ResourceStore.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Resources;
-
-namespace Resources
+﻿namespace Resources
 {
     /// <summary>
     /// Realization of <see cref="IResourceStore"/>.
     /// </summary>
     public class ResourceStore : IResourceStore
     {
-        private readonly ResourceManager _resourceManager;
-
-        public ResourceStore()
-        {
-            _resourceManager = new ResourceManager("ExceptionMessages", typeof(ResourceStore).Assembly);
-        }
-
         /// <inheritdoc />
         public string GetExceptionMessage(string resourceName)
         {
-            return _resourceManager.GetString(resourceName);
+            return ExceptionMessages.ResourceManager.GetString(resourceName);
         }
     }
 }
